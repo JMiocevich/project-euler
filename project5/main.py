feed41685abf5740e5b0dce1808552ed0f9a0b80
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-06-27 19:39:06 +0800

    prod 5

--- project5/main.py
+++ project5/main.py
@@ -1,26 +1,3 @@
 
 
-def isPalindrone(x):
-    value = str(x)
-    reversedValue = value[::-1]
-    if value == reversedValue:
-        print("true")
-        return True
-    else: 
-        return False
-
-array = []
-
-for i in range(100,999):
-    for j in range(100,999):
-        value = i*j
-
-        if isPalindrone(value):
-            array.append(int(value))
-
-
-print(max(array))
-
-            
-
 
